03760a79298cc341091c0c23f0e268364e40c9d0
admin <davy.ccm@gmail.com>
2024-05-15 23:14:23 +0800

    ocp build

--- src/main/java/com/demo/Saml2SsoSpringbokKeycloakApplication.java
+++ src/main/java/com/demo/Saml2SsoSpringbokKeycloakApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Saml2SsoSpringbokKeycloakApplication {
 
 	public static void main(String[] args) {
+
+		System.setProperty("javax.net.debug", "all");
 		SpringApplication.run(Saml2SsoSpringbokKeycloakApplication.class, args);
 	}
 
